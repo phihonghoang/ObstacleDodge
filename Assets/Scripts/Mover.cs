737bcba1cf5f56d069bacc1012025ef25a959953
phihonghoang <116082083+phihonghoang@users.noreply.github.com>
2024-11-10 23:10:07 +0100

    Added cinemachine

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Transform in inspector
-        // transform.Translate(1, 0, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yVlaue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        // Transform in inspector
+        transform.Translate(xValue, yVlaue, zValue);
     }
 }
